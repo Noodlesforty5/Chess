5633aa09d512a85b23e7f0b3c95fa4f9f338dcd5
Cthom <cthompsonlds@gmail.com>
2025-01-30 12:38:06 -0700

    Preparing to start Phase 1

--- shared/src/main/java/MoveCalculators/KingMoveCalculator.java
+++ shared/src/main/java/MoveCalculators/KingMoveCalculator.java
@@ -9,6 +9,6 @@ import java.util.Collection;
 public class KingMoveCalculator extends MoveCalculators{
     public static Collection<ChessMove> getMoves(ChessBoard board, ChessPosition myPosition) {
         int [][] posMoves = {{1,1},{0,1},{1,0},{-1,0},{0,-1},{-1,-1},{-1,1},{1,-1}};
-    return MoveCalculators.generateMoves(board,myPosition,posMoves);
+        return MoveCalculators.generateMoves(board,myPosition,posMoves);
     }
 }
