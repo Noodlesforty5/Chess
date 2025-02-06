1c333db83892eb58f5fd684156c1949fdd09bf53
Cthom <cthompsonlds@gmail.com>
2025-02-05 18:46:06 -0700

    isInStalemate

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -19,6 +19,7 @@ public class ChessGame {
      * @return Which team's turn it is
      */
     public TeamColor getTeamTurn() {
+
         return turn;
     }
 
@@ -28,6 +29,7 @@ public class ChessGame {
      * @param team the team whose turn it is
      */
     public void setTeamTurn(TeamColor team) {
+
         this.turn = team;
     }
 
