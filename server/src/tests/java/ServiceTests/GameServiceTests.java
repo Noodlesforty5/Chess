e10aec651619bb91fea90ba5ef76ce5c6549153c
Cthom <cthompsonlds@gmail.com>
2025-03-12 18:56:04 -0600

    UserServiceTests

--- server/src/tests/java/ServiceTests/GameServiceTests.java
+++ server/src/tests/java/ServiceTests/GameServiceTests.java
@@ -1,4 +1,28 @@
 package ServiceTests;
 
+import Service.GameService;
+import dataaccess.GameDAO;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+
 public class GameServiceTests {
+    private static GameDAO gameDAO;
+    private static GameService gameService;
+
+
+    @Test
+    @DisplayName("List Games Positive Test")
+    @Order(1)
+    public void ListGamesPosTest(){
+
+
+    }
+    @Test
+    @DisplayName("List Games Negative Test")
+    @Order(2)
+    public void ListGamesNegTest(){
+
+    }
 }
--- server/src/tests/java/ServiceTests/UserServiceTests.java
+++ server/src/tests/java/ServiceTests/UserServiceTests.java
@@ -1,4 +1,88 @@
 package ServiceTests;
 
+
+import Service.UserService;
+import dataaccess.*;
+import org.junit.jupiter.api.*;
+import records.UserData;
+
+
+
 public class UserServiceTests {
+    private static UserDAO userDAO;
+    private static AuthDAO authDAO;
+    private static UserService userService;
+
+    @AfterAll
+    public static void clear(){
+        authDAO.clear();
+        userDAO.clear();
+    }
+    @BeforeAll
+    public static void init(){
+        userDAO = new MemoryUserDAO();
+        authDAO = new MemoryAuthDAO();
+        userService = new UserService(userDAO, authDAO);
+
+    }
+    @Test
+    @DisplayName("Register Positive Test")
+    @Order(1)
+    public void RegisterPosTest() throws DataAccessException {
+        UserData user = new UserData("user", "password", "email.email@email");
+        userService.createUser(user);
+        assert userDAO.authenticateUser("user", "password");
+
+    }
+    @Test
+    @DisplayName("Register Negative Test")
+    @Order(2)
+    public void ListGamesNegTest(){
+
+    }
+    @Test
+    @DisplayName("Register Positive Test")
+    @Order(3)
+    public void LogoutPosTest() throws DataAccessException {
+        UserData user = new UserData("user", "password", "email.email@email");
+        userService.createUser(user);
+        assert userDAO.authenticateUser("user", "password");
+
+    }
+    @Test
+    @DisplayName("Register Negative Test")
+    @Order(4)
+    public void LogoutNegTest(){
+
+    }
+    @Test
+    @DisplayName("Register Positive Test")
+    @Order(5)
+    public void LoginPosTest() throws DataAccessException {
+        UserData user = new UserData("user", "password", "email.email@email");
+        userService.createUser(user);
+        assert userDAO.authenticateUser("user", "password");
+
+    }
+    @Test
+    @DisplayName("Register Negative Test")
+    @Order(6)
+    public void LoginNegTest(){
+
+    }
+    @Test
+    @DisplayName("Register Positive Test")
+    @Order(7)
+    public void GetAuthTest() throws DataAccessException {
+        UserData user = new UserData("user", "password", "email.email@email");
+        userService.createUser(user);
+        assert userDAO.authenticateUser("user", "password");
+
+    }
+    @Test
+    @DisplayName("Register Negative Test")
+    @Order(8)
+    public void GetAuthNegTest(){
+
+    }
 }
